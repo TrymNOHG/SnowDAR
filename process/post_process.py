046bae44e00a480e6cd684d2c007b47204f4005c
TrymNOHG <trym.gudvangen@gmail.com>
2025-05-01 18:07:29 +0200

    fix: problems with processing

--- process/post_process.py
+++ process/post_process.py
@@ -12,12 +12,12 @@ def update_annotation(file_path, y_percent):
         if len(vals) != 6:
             continue
         else:
-            x, y, w, h = vals[1:-1]
+            x, y, w, h, c = vals[1:]
             new_line = vals[0] + " "
             new_line += x + " "
             new_y =  float(y) * (1 - y_percent) + y_percent
             new_line += str(new_y) + " "
-            new_line += w + " " + h + "\n"
+            new_line += w + " " + h + " " + c + "\n"
             new_lines.append(new_line)
     
     with open(file_path, 'w', encoding='utf-8') as f:
